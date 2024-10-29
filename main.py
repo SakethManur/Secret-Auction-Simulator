184109904179bb279b22bda771077436289b5d08
SakethManur <159404215+SakethManur@users.noreply.github.com>
2024-10-29 21:52:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import art
 
 print(art.logo)
-# TODO-1: Ask the user for input
 def compare(bids):
     winner=""
     highest_bid=0
@@ -15,23 +14,18 @@ def compare(bids):
 
 
     print(f"The winner is {winner} with a bid of ${highest_bid}")
-# TODO-2: Save data into dictionary {name: price}
-user_name = input("Please enter your name:")
-user_bid = int(input("Please enter the bid:$"))
 
 bids={}
-# TODO-3: Whether if new bids need to be added
-should_continue=input("Are there any other bids? if yes then type 'yes' or type 'no'\n")
+should_continue=input("Are there any bids? if yes then type 'yes' or type 'no'\n") #asks for if there are any bids
 continue_bids=True
 while continue_bids:
-    user_name = input("Please enter your name:")
-    user_bid = int(input("Please enter the bid:$"))
+    user_name = input("Please enter your name:")#asks the bidder's name
+    user_bid = int(input("Please enter the bid:$"))#asks the biddes's bid 
     bids[user_name] = user_bid
     should_continue = input("Are there any other bids? if yes then type 'yes' or type 'no'\n")
     if should_continue == "no" :
         continue_bids=False
-        compare(bids)
+        compare(bids)#calls the function above to give the final result
     elif should_continue == "yes":
-        print("\n" * 50)
-# TODO-4: Compare bids in dictionary
+        print("\n" * 50)#prints fifty empty lines to hide the bids of other bidders
 
